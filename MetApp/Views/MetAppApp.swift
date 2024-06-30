c41c6f9e0eb0285a6f4ae54c92aa7c6d5f752ec8
Sravan <sravan.karuturi@strivr.com>
2024-06-29 17:03:54 -0700

    Removed SwiftData

--- MetApp/Views/MetAppApp.swift
+++ MetApp/Views/MetAppApp.swift
@@ -6,27 +6,18 @@
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct MetAppApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+    init() {
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
             MetalView()
         }
-        .modelContainer(sharedModelContainer)
+        
     }
 }
